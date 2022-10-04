e8d038ac79f5f21299687b24a4f9c3655427ad02
Sarah Abou Dalal <sarahaboudalal@gmail.com>
2022-10-04 16:32:34 +0300

    done

--- src/reducer.js
+++ src/reducer.js
@@ -1,1 +1,35 @@
 // add code snippets from README
+let state;
+
+function reducer(state = { count: 0 }, action) {
+  switch (action.type) {
+    case "counter/increment":
+      return { count: state.count + 1 };
+    default:
+      return state;
+  }
+} 
+
+function dispatch(action) {
+    state = reducer(state, action);
+    render()
+ } 
+
+ function render() {
+    let container = document.getElementById("container");
+    container.textContent = state.count;
+  } 
+
+  
+  let button = document.getElementById("button");
+
+  button.addEventListener("click", () => {
+    dispatch({ type: "counter/increment" });
+  }); 
+
+  
+dispatch({ type: "@@INIT" }); 
+// reducer({ count: 0 }, { type: "counter/increment" });
+// dispatch({type: 'counter/increment'}) 
+// dispatch({type: 'counter/increment'}) 
+// dispatch({type: 'counter/increment'}) 
